52b857b7e27c570171d33e733d251fb7a2b4c056
Kristin Yen <KristinYen@Kristins-MacBook-Pro.local>
2017-10-17 08:36:45 -0400

    Switching server back

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ var path = require( "path" );
 var port;
 var server;
 
+var port = process.env.port || 3030;
+
 app.get( "*", function appGet( req, res ){
     res.sendFile( path.resolve( "./index.html" ) );
 } );
 
 server = http.createServer( app );
-server.listen( process.env.port || 3030, function serverListen(){
+server.listen( port, function serverListen(){
     /* eslint-disable no-console */
     console.log( "Listening on porto: ", port );
 } );
